33df7d67553fb467c2bf291b7c8d8c54b5e5ebd6
Jannchie <jannchie@gmail.com>
2022-08-07 03:41:24 +0900

    chore: update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
-
+import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.tsx"),
+      name: "roku-ui",
+      fileName: "roku-ui",
+    },
+  },
 });
