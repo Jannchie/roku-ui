d5af1749e5987e1b4fa7626099c45275bb0bd5f3
Jannchie <jannchie@gmail.com>
2022-08-06 23:18:35 +0900

    feat: combobox

--- src/stories/components/RCombobox.stories.tsx
+++ src/stories/components/RCombobox.stories.tsx
@@ -1,15 +1,20 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { RCombobox } from "../../components/RCombobox";
+import { RCombobox } from "../..";
 export default {
   title: "Example/RCombobox",
   component: RCombobox,
 } as ComponentMeta<typeof RCombobox>;
 
 const Template: ComponentStory<typeof RCombobox> = (args) => (
-  <div className="flex justify-center">
+  <div className="flex justify-center h-32">
     <RCombobox {...args} />
   </div>
 );
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  data: [
+    { id: 1, name: "Durward Reynolds" },
+    { id: 2, name: "Linda Mckee" },
+  ],
+};
