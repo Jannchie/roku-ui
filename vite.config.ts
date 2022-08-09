f8194d7a3f697ac4dc6934f80eeee720bf203a22
Jannchie <jannchie@gmail.com>
2022-08-09 19:28:57 +0900

    chore: build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,22 @@ import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), dts({ outputDir: "dist/types" })],
   build: {
+    target: "modules",
     lib: {
       entry: resolve(__dirname, "src/index.tsx"),
-      name: "roku-ui",
+      name: "RokuUI",
       fileName: "roku-ui",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
     },
   },
 });
