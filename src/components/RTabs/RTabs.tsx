0296655442c875052f973036aa03d99d805288c5
Jannchie <jannchie@gmail.com>
2022-08-11 04:35:55 +0900

    feat: Self-implementing tab

--- src/components/RTabs/RTabs.tsx
+++ src/components/RTabs/RTabs.tsx
@@ -1,93 +1,158 @@
 import "./style.css";
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { FC, forwardRef, ReactNode, useEffect, useRef, useState } from "react";
 import { colorClass, Colors } from "../../utils/colors";
-import { Tab, Transition } from "@headlessui/react";
+import { Transition } from "@headlessui/react";
 import classNames from "classnames";
+import { TransitionMotion, spring } from "react-motion";
 
-export function RTabs({
+function Item({ children }: { label: ReactNode; children?: ReactNode }) {
+  return <>{children}</>;
+}
+
+function List({
   data,
+  color,
+  type,
   selectedIndex,
   onChange,
-  type = "indicator",
-  color = "primary",
-  className,
 }: {
   data: {
     key: ReactNode;
     value: ReactNode;
   }[];
   selectedIndex: number;
+  color: Colors;
+  type: "fill" | "indicator";
   onChange: (index: number) => void;
-  className?: string;
-  type?: "fill" | "indicator";
-  color?: Colors;
 }) {
   const tabList = useRef<HTMLDivElement>(null);
   const [indicatorStyle, setIndicatorStyle] = useState<{
     width: number;
     left: number;
   }>({ width: 0, left: 0 });
-  useEffect(() => {
-    if (tabList.current) {
-      const tabBtn = tabList.current.children[
-        selectedIndex
-      ] as HTMLButtonElement;
-      setIndicatorStyle(() => ({
-        width: tabBtn.offsetWidth,
-        left: tabBtn.offsetLeft,
-      }));
-    }
-  }, [selectedIndex]);
   const indicatorColor = colorClass({
     bg: color,
   });
   const textColor = colorClass({
     text: color,
   });
+  useEffect(() => {
+    if (tabList.current) {
+      const tabBtn = tabList.current.children[
+        selectedIndex
+      ] as HTMLButtonElement;
+      if (tabBtn) {
+        setIndicatorStyle(() => ({
+          width: tabBtn.offsetWidth,
+          left: tabBtn.offsetLeft,
+        }));
+      }
+    }
+  }, [selectedIndex]);
   return (
-    <div className={className}>
-      <Tab.Group selectedIndex={selectedIndex} onChange={onChange}>
-        <Tab.List ref={tabList} className="r-tab-list">
-          {data.map((d, i) => (
-            <Tab
-              key={i}
-              className={({ selected }) =>
-                selected
-                  ? type === "indicator"
-                    ? classNames(textColor)
-                    : classNames("text-white", indicatorColor)
-                  : ""
-              }
-              children={d.key}
-            ></Tab>
-          ))}
-        </Tab.List>
-        {type === "indicator" && (
-          <div
-            className={classNames(
-              "transition-all absolute h-1 rounded-md bg-primary-500",
-              indicatorColor
-            )}
-            style={indicatorStyle}
-          ></div>
-        )}
-        <Tab.Panels className="r-tab-panels dark:text-white mt-2">
-          {data.map((d, i) => (
-            <Tab.Panel key={i}>
-              <Transition
-                key={i}
-                show={i === selectedIndex}
-                appear
-                enter="transition-all ease-out duration-300"
-                enterFrom="opacity-0 mt-4"
-                enterTo="opacity-100 mt-0"
-              >
-                {d.value}
-              </Transition>
-            </Tab.Panel>
-          ))}
-        </Tab.Panels>
-      </Tab.Group>
-    </div>
+    <>
+      <div ref={tabList} className="r-tab-list">
+        {data.map((d, i) => (
+          <button
+            onClick={() => {
+              onChange(i);
+            }}
+            key={i}
+            className={
+              i === selectedIndex
+                ? type === "indicator"
+                  ? classNames(textColor)
+                  : classNames("text-white", indicatorColor)
+                : ""
+            }
+            children={d.key}
+          ></button>
+        ))}
+      </div>
+      {type === "indicator" && (
+        <div
+          className={classNames("r-tab-indicator", indicatorColor)}
+          style={indicatorStyle}
+        ></div>
+      )}
+    </>
   );
 }
+type RTabsProps = {
+  selectedIndex: number;
+  onChange: (index: number) => void;
+  className?: string;
+  type?: "fill" | "indicator";
+  color?: Colors;
+  children: ReactNode;
+};
+
+export function RTabRoot(props: RTabsProps) {
+  const {
+    selectedIndex,
+    onChange,
+    type = "indicator",
+    color = "primary",
+    className,
+    children,
+  } = props;
+  const data = getData();
+  return (
+    <div className={classNames(className, "relative")}>
+      <List
+        data={data}
+        type={type}
+        color={color}
+        selectedIndex={selectedIndex}
+        onChange={onChange}
+      />
+      <div className="r-tab-panels dark:text-white mt-2 h-32">
+        {data.map((d, i) => (
+          <Transition
+            key={i}
+            show={i === selectedIndex}
+            appear
+            enter="transition-all ease-out duration-300 absolute delay-300"
+            enterFrom="opacity-0 mt-4"
+            enterTo="opacity-100 mt-0"
+            leave="transition-all ease-in duration-300 absolute"
+            leaveFrom="opacity-100 mt-0"
+            leaveTo="opacity-0 mt-4"
+          >
+            {d.value}
+          </Transition>
+        ))}
+      </div>
+    </div>
+  );
+
+  function getData() {
+    const data = [];
+    if ("children" in props) {
+      if (children) {
+        if (Array.isArray(props.children)) {
+          props.children.map((tab) => {
+            if (tab.props) {
+              data.push({
+                key: tab.props.label,
+                value: tab.props.children,
+              });
+            }
+          });
+        } else {
+          if (typeof children === "object" && "props" in children) {
+            if (children.props) {
+              data.push({
+                key: children.props.label,
+                value: children.props.children,
+              });
+            }
+          }
+        }
+      }
+    }
+    return data;
+  }
+}
+
+export const RTabs = Object.assign(RTabRoot, { Item, List });
--- src/stories/components/RTabs.stories.tsx
+++ src/stories/components/RTabs.stories.tsx
@@ -1,7 +1,6 @@
-import { Tab } from "@headlessui/react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { useState } from "react";
-import { RBtn, RCard, RNotice, RTabs } from "../..";
+import { RTabs } from "../..";
 export default {
   title: "Example/RTabs",
 } as ComponentMeta<typeof RTabs>;
@@ -11,60 +10,18 @@ const Template: ComponentStory<typeof RTabs> = () => {
   return (
     <>
       <RTabs
-        type={"fill"}
+        type={"indicator"}
         color={"teal"}
         selectedIndex={selectedIndex}
         onChange={setSelectedIndex}
-        data={[
-          {
-            key: "Page 1",
-            value: (
-              <RCard>
-                <div className="p-2">Content1</div>
-              </RCard>
-            ),
-          },
-          {
-            key: "Page 2",
-            value: (
-              <RCard>
-                <RNotice title="Test Notice" desc="Test Desc"></RNotice>
-              </RCard>
-            ),
-          },
-          {
-            key: "Page 3",
-            value: <div className="p-2">Content3</div>,
-          },
-        ]}
-      ></RTabs>
-      <RTabs
-        selectedIndex={selectedIndex}
-        onChange={setSelectedIndex}
-        type={"indicator"}
-        data={[
-          {
-            key: "Page 1",
-            value: (
-              <RCard>
-                <div className="p-2">Content1</div>
-              </RCard>
-            ),
-          },
-          {
-            key: "Page 2",
-            value: (
-              <RCard>
-                <RNotice title="Test Notice" desc="Test Desc"></RNotice>
-              </RCard>
-            ),
-          },
-          {
-            key: "Page 3",
-            value: <div className="p-2">Content3</div>,
-          },
-        ]}
-      ></RTabs>
+      >
+        <RTabs.Item label="Tab 1">
+          <div className="p-2">Content 1</div>
+        </RTabs.Item>
+        <RTabs.Item label="Tab 2">
+          <div className="p-2">Content 2</div>
+        </RTabs.Item>
+      </RTabs>
     </>
   );
 };
