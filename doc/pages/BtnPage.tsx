2508ecf90e74359a09d8b44661fd24655d400145
Jannchie <jannchie@gmail.com>
2022-08-30 01:28:50 +0900

    feat: counter fill

--- doc/pages/BtnPage.tsx
+++ doc/pages/BtnPage.tsx
@@ -122,22 +122,25 @@ const AllTemplate = () => (
 );
 
 function CounterBtnTemplate() {
+  const [fillA, setFillA] = useState(false);
+  const [fillB, setFillB] = useState(false);
+  const [fillC, setFillC] = useState(false);
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
       <div style={{ display: 'flex', gap: 8 }}>
-        <Btn.Counter size="sm" value={2} icon="chat_bubble" />
-        <Btn.Counter size="sm" color="red" value={4} icon="favorite" />
-        <Btn.Counter size="sm" color="green" value={7} icon="reply" />
+        <Btn.Counter size="sm" value={2} icon="chat_bubble" fill={fillA} onClick={() => { setFillA(!fillA); }} />
+        <Btn.Counter size="sm" color="red" value={4} icon="favorite" fill={fillB} onClick={() => { setFillB(!fillB); }} />
+        <Btn.Counter size="sm" color="green" value={7} icon="reply" fill={fillC} onClick={() => { setFillC(!fillC); }} />
       </div>
       <div style={{ display: 'flex', gap: 8 }}>
-        <Btn.Counter size="md" value={2} icon="chat_bubble" />
-        <Btn.Counter size="md" color="red" value={4} icon="favorite" />
-        <Btn.Counter size="md" color="green" value={7} icon="reply" />
+        <Btn.Counter size="md" value={2} icon="chat_bubble" fill={fillA} onClick={() => { setFillA(!fillA); }} />
+        <Btn.Counter size="md" color="red" value={4} icon="favorite" fill={fillB} onClick={() => { setFillB(!fillB); }} />
+        <Btn.Counter size="md" color="green" value={7} icon="reply" fill={fillC} onClick={() => { setFillC(!fillC); }} />
       </div>
       <div style={{ display: 'flex', gap: 8 }}>
-        <Btn.Counter size="lg" value={2} icon="chat_bubble" />
-        <Btn.Counter size="lg" color="red" value={4} icon="favorite" />
-        <Btn.Counter size="lg" color="green" value={7} icon="reply" />
+        <Btn.Counter size="lg" value={2} icon="chat_bubble" fill={fillA} onClick={() => { setFillA(!fillA); }} />
+        <Btn.Counter size="lg" color="red" value={4} icon="favorite" fill={fillB} onClick={() => { setFillB(!fillB); }} />
+        <Btn.Counter size="lg" color="green" value={7} icon="reply" fill={fillC} onClick={() => { setFillC(!fillC); }} />
       </div>
     </div>
   );
