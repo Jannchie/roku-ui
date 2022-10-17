9594357faa274f88fc0d1c478456940253a7b3aa
Jannchie <jannchie@gmail.com>
2022-10-17 19:18:56 +0900

    chore: beautify article demo

--- doc/demo/Article.demo.tsx
+++ doc/demo/Article.demo.tsx
@@ -1,21 +1,23 @@
 import { Article } from '../../src'
 export default function Demo () {
-  return <Article>
-    <h1>标题1</h1>
-    <h2>标题2</h2>
-    <h3>标题3</h3>
-    <p>段落</p>
-    <code>代码</code>
-    <pre>代码块</pre>
-    <ul>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-    </ul>
-    <ol>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-    </ol>
-  </Article>
+  return (
+    <Article style={{ maxWidth: '80ch' }}>
+      <h1>标题1</h1>
+      <h2>标题2</h2>
+      <h3>标题3</h3>
+      <p>段落</p>
+      <code>代码</code>
+      <pre>代码块</pre>
+      <ul>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+      </ul>
+      <ol>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+      </ol>
+    </Article>
+  )
 }
