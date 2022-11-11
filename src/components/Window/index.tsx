c2998d9f0913ff0c8966632ab2ed369b3a8a2096
Jannchie <jannchie@gmail.com>
2022-11-11 23:34:17 +0900

    fix: children selector

--- src/components/Window/index.tsx
+++ src/components/Window/index.tsx
@@ -1,19 +1,22 @@
+import classNames from 'classnames'
 import { HTMLAttributes, ReactNode } from 'react'
+import { Colors } from '../../utils/colors'
 import { Panel } from '../Panel'
 
 export function Window ({
   children,
-  className,
+  color,
   ...props
 }: {
   children: ReactNode
+  color: Colors
 } & HTMLAttributes<HTMLDivElement>) {
   return (
-    <Panel border>
+    <Panel border color={color} className={classNames(props.className)} {...props}>
       <div className="flex gap-2 p-2">
-        <div className="w-3 h-3 bg-danger-1 rounded-full" />
-        <div className="w-3 h-3 bg-warning-1 rounded-full" />
-        <div className="w-3 h-3 bg-success-1 rounded-full" />
+        <div className="w-3 h-3 bg-red-5 rounded-full" />
+        <div className="w-3 h-3 bg-yellow-5 rounded-full" />
+        <div className="w-3 h-3 bg-green-5 rounded-full" />
       </div>
       <div className="p-2">body</div>
     </Panel>
