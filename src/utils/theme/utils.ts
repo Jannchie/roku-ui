d2dfa39c2daecdda48c08a90932b5075e1315b5e
Jannchie <jannchie@gmail.com>
2022-11-14 18:53:30 +0900

    feat: get css string

--- src/utils/theme/utils.ts
+++ src/utils/theme/utils.ts
@@ -8,26 +8,31 @@ export function registTheme (name: string, theme: Theme) {
       // already exist
       return
     }
-    const items = []
-    const regexp = /hsl\(([0-9]{1,}).?[0-9]*,\s*([0-9]{1,}).?[0-9]*%,\s*([0-9]{1,}).?[0-9]*%\)/
-    for (const [key, value] of Object.entries(theme)) {
-      const c = hsl(value)
-      if (c) {
-        // extract hsl
-        console.log(c.formatHsl().match(regexp), c.formatHsl())
-        items.push(`--r-${key}: ${c.formatHsl().match(regexp)?.slice(1, 4).map((d, i) => i === 0 ? d : d + '%').join(', ') ?? ''};`)
-        for (const level of [1, 2, 3]) {
-          let newColor = c
-          if (level === 1) { newColor = c.brighter(level * theme.k) }
-          if (level === 3) { newColor = c.darker(level * theme.k) }
-          items.push(`--r-${key}-${level}: ${newColor.formatHsl().match(regexp)?.slice(1, 4).map((d, i) => i === 0 ? d : d + '%').join(', ') ?? ''};`)
-        }
-      }
-    }
+    const cssStr = getCSS(theme, name)
     style = document.createElement('style')
     style.id = `theme-${name}`
-    style.innerHTML = `
-    [data-theme="${name}"] {${items.join('')}}`.replace(/\s+/g, ' ')
+    style.innerHTML = cssStr
     document.head.appendChild(style)
   }
+}
+
+export function getCSS (theme: Theme, name: string) {
+  const items = []
+  const regexp = /hsl\(([0-9]{1,}).?[0-9]*,\s*([0-9]{1,}).?[0-9]*%,\s*([0-9]{1,}).?[0-9]*%\)/
+  for (const [key, value] of Object.entries(theme)) {
+    const c = hsl(value)
+    if (c) {
+      // extract hsl
+      console.log(c.formatHsl().match(regexp), c.formatHsl())
+      items.push(`--r-${key}: ${c.formatHsl().match(regexp)?.slice(1, 4).map((d, i) => i === 0 ? d : d + '%').join(', ') ?? ''};`)
+      for (const level of [1, 2, 3]) {
+        let newColor = c
+        if (level === 1) { newColor = c.brighter(level * theme.k) }
+        if (level === 3) { newColor = c.darker(level * theme.k) }
+        items.push(`--r-${key}-${level}: ${newColor.formatHsl().match(regexp)?.slice(1, 4).map((d, i) => i === 0 ? d : d + '%').join(', ') ?? ''};`)
+      }
+    }
+  }
+  const cssStr = `[data-theme="${name}"] {${items.join('')}}`.replace(/\s+/g, ' ')
+  return cssStr
 }
