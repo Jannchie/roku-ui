ef09f3ef71dc06e45c988083be56116e8be3e51b
Jannchie <jannchie@gmail.com>
2023-02-10 17:56:19 +0900

    feat: add other props for container

--- src/components/Layout/Container/Container.tsx
+++ src/components/Layout/Container/Container.tsx
@@ -1,9 +1,10 @@
-import { type ReactNode } from 'react'
+import classNames from 'classnames'
+import { type HTMLAttributes, type ReactNode } from 'react'
 import './Container.css'
 
-export function Container ({ children }: { children: ReactNode }) {
+export function Container ({ children, className, ...others }: { children: ReactNode } & HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className="r-container">
+    <div className={classNames('r-container', className)} {...others}>
       {children}
     </div>
   )
