4b7e08eb9a1c81dde7fd9c54a64c2f4e260db0b2
Jannchie <jannchie@gmail.com>
2023-02-14 11:18:13 +0900

    feat: use @jannchie/eslint-config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,7 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/jsx-runtime',
-    'standard-with-typescript',
-    'plugin:jsx-a11y/recommended',
+    '@jannchie',
   ],
   settings: {
     react: {
@@ -19,31 +15,5 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['tsconfig.json'],
-  },
-  plugins: [
-    'react',
-    'jsx-a11y',
-  ],
-  rules: {
-    indent: ['warn', 2, { SwitchCase: 1 }],
-    '@typescript-eslint/indent': ['warn', 2, { SwitchCase: 1 }],
-    'jsx-quotes': ['warn', 'prefer-double'],
-    '@typescript-eslint/no-invalid-void-type': 'off',
-    'space-before-function-paren': 'off',
-    '@typescript-eslint/space-before-function-paren': 'warn',
-    'comma-dangle': ['warn', 'always-multiline'],
-    '@typescript-eslint/comma-dangle': ['warn', 'always-multiline'],
-    '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'react/self-closing-comp': ['warn', { component: true, html: true }],
-    'react/jsx-sort-props': [
-      'warn',
-      {
-        noSortAlphabetically: true,
-        callbacksLast: true,
-        reservedFirst: true,
-        shorthandFirst: true,
-      },
-    ],
   },
 }
