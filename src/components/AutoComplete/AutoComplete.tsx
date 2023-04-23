8e1717e52e96771fba883608b21e55b4c12acecb
Jannchie <jannchie@gmail.com>
2023-04-24 00:57:08 +0900

    fix: auto complete

--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -56,6 +56,7 @@ export function AutoComplete<T> ({
             </svg>
           </Btn>
         )}
+        className="r-combobox-input"
         color={color}
         value={query}
         onClick={() => { setFocused(true) }}
