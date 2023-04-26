9da1f7da657f11851a15e2ac280058698337cdee
Jannchie <jannchie@gmail.com>
2023-04-26 23:21:45 +0900

    fix: textarea style

--- doc/demo/Textarea.demo.tsx
+++ doc/demo/Textarea.demo.tsx
@@ -4,6 +4,6 @@ import { Textarea } from '../../src'
 export default function Demo () {
   const [value, setValue] = useState('')
   return (
-    <Textarea value={value} setValue={setValue} color="success" placeholder="请输入文本"/>
+    <Textarea style={{ width: 400, height: 300 }} value={value} setValue={setValue} color="success" placeholder="请输入文本"/>
   )
 }
