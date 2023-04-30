1347762f285df5b5be877f74647b9a548a6a897b
Jannchie <jannchie@gmail.com>
2023-04-30 13:29:06 +0900

    doc: use theme toggle

--- doc/router.tsx
+++ doc/router.tsx
@@ -1,10 +1,11 @@
 import HomeMD from './markdown/home.md?raw'
 import { Page } from './components/Page'
-import { TablerMoon } from '@roku-ui/icons-tabler'
+import { Tabler123, TablerAnchor, TablerArticle, TablerCircleCheck, TablerCursorText, TablerHome, TablerInputSearch, TablerLayoutNavbar, TablerMessage, TablerMoon, TablerRectangle, TablerRectangleFilled, TablerTag, TablerTypography, TablerUserCircle, TablerWindow } from '@roku-ui/icons-tabler'
+import { MaterialSymbolsArrowRightAlt, MaterialSymbolsArticle, MaterialSymbolsCampaign, MaterialSymbolsDriveFileRenameOutline, MaterialSymbolsExpandMore, MaterialSymbolsFormatSize, MaterialSymbolsLandscape, MaterialSymbolsLiveHelp, MaterialSymbolsMarkChatUnread, MaterialSymbolsNotifications, MaterialSymbolsRadio, MaterialSymbolsScience, MaterialSymbolsSmartphone, MaterialSymbolsStar, MaterialSymbolsTab, MaterialSymbolsToggleOn, MaterialSymbolsViewDay, MaterialSymbolsWebAsset, MaterialSymbolsWindow } from '@roku-ui/icons-material-symbols'
 export const router = [{
   path: '/',
   title: '主页',
-  icon: 'home',
+  icon: <TablerHome />,
   element: <Page md={HomeMD} />,
 }, {
   path: 'theme-toggle',
@@ -14,172 +15,172 @@ export const router = [{
 }, {
   path: 'auto-complete',
   title: '补选框（Auto Complete）',
-  icon: 'inventory_2',
+  icon: <TablerInputSearch />,
   element: <Page />,
 }, {
   path: 'anchor',
   title: '锚点（Anchor）',
-  icon: 'anchor',
+  icon: <TablerAnchor />,
   element: <Page />,
 }, {
   path: 'typography',
   title: '排版（Typography）',
-  icon: 'title',
+  icon: <TablerTypography />,
   element: <Page />,
 }, {
   path: 'panel',
   title: '面板（Panel）',
-  icon: 'table_bar',
+  icon: <TablerRectangle />,
   element: <Page />,
 }, {
   path: 'appbar',
   title: ' 应用栏（Appbar）',
-  icon: 'settop_component',
+  icon: <TablerLayoutNavbar />,
   element: <Page />,
 }, {
   path: 'btn',
   title: '按钮（Btn）',
-  icon: 'crop_16_9',
+  icon: <TablerRectangleFilled />,
   element: <Page />,
 }, {
   path: 'result',
   title: '结果（Result）',
-  icon: 'check_circle',
+  icon: <TablerCircleCheck />,
   element: <Page />,
 }, {
   path: 'tag',
   title: '标签 (Tag)',
-  icon: 'label',
+  icon: <TablerTag />,
   element: <Page />,
 }, {
   path: 'textarea',
   title: '文本框（Textarea）',
-  icon: 'subject',
+  icon: <TablerArticle />,
   element: <Page />,
 }, {
   path: 'text-field',
   title: '输入框（Text Field）',
-  icon: 'edit',
+  icon: <TablerCursorText />,
   element: <Page />,
 }, {
   path: 'editable-line',
   title: '可编行（Editable Line）',
-  icon: 'drive_file_rename_outline',
+  icon: <MaterialSymbolsDriveFileRenameOutline />,
   element: <Page />,
 }, {
   path: 'dynamic-value',
   title: '灵动数值（Dynamic Value）',
-  icon: '123',
+  icon: <Tabler123 />,
   element: <Page />,
 }, {
   path: 'dynamic-island',
   title: '灵动岛（Dynamic Island）',
-  icon: 'landscape',
+  icon: <MaterialSymbolsLandscape />,
   element: <Page />,
 }, {
   path: 'tabs',
   title: '选项卡（Tabs）',
-  icon: 'tab',
+  icon: <MaterialSymbolsTab />,
   element: <Page />,
 }, {
   path: 'avatar',
   title: '头像（Avatar）',
-  icon: 'account_circle',
+  icon: <TablerUserCircle />,
   element: <Page />,
 }, {
   path: 'comment',
   title: '评论 (Comment)',
-  icon: 'comment',
+  icon: <TablerMessage />,
   element: <Page />,
 }, {
   path: 'holy-grail',
   title: '圣杯 (HolyGrail)',
-  icon: 'view_day',
+  icon: <MaterialSymbolsViewDay />,
   element: <Page />,
 }, {
   path: 'article',
   title: '文章（Article）',
-  icon: 'article',
+  icon: <MaterialSymbolsArticle />,
   element: <Page />,
 }, {
   path: 'notice',
   title: '通知（Notice）',
-  icon: 'campaign',
+  icon: <MaterialSymbolsCampaign />,
   element: <Page />,
 }, {
   path: 'notification',
   title: '通知（Notification）',
-  icon: 'notifications',
+  icon: <MaterialSymbolsNotifications />,
   element: <Page />,
 }, {
   path: 'badge',
   title: '徽章（Badge）',
-  icon: 'mark_chat_unread',
+  icon: <MaterialSymbolsMarkChatUnread />,
   element: <Page />,
 }, {
   path: 'modal',
   title: '模态框（Modal）',
-  icon: 'web_asset',
+  icon: <MaterialSymbolsWebAsset />,
   element: <Page />,
 }, {
   path: 'popover',
   title: '溢出框（Popover）',
-  icon: 'live_help',
+  icon: <MaterialSymbolsLiveHelp />,
   element: <Page />,
 }, {
   path: 'switch',
   title: '开关（Switch）',
-  icon: 'toggle_on',
+  icon: <MaterialSymbolsToggleOn />,
   element: <Page />,
 }, {
   path: 'window',
   title: '窗体（Window）',
-  icon: 'wysiwyg',
+  icon: <TablerWindow />,
   element: <Page />,
 }, {
   path: 'phone',
   title: '手机模型（Phone）',
-  icon: 'smartphone',
+  icon: <MaterialSymbolsSmartphone />,
   element: <Page />,
 }, {
   path: 'steps',
   title: '步骤（Steps）',
-  icon: 'arrow_right_alt',
+  icon: <MaterialSymbolsArrowRightAlt />,
   element: <Page />,
 }, {
   path: 'collapse',
   title: '折叠面板（Collapse）',
-  icon: 'expand_more',
+  icon: <MaterialSymbolsExpandMore />,
   element: <Page />,
 }, {
   path: 'icon',
   title: '图标（Icon）',
-  icon: 'dentistry',
+  icon: <MaterialSymbolsScience />,
   element: <Page />,
 }, {
   path: 'rating',
   title: '评分（Rating）',
-  icon: 'star',
+  icon: <MaterialSymbolsStar />,
   element: <Page />,
 }, {
   path: 'radio',
   title: '单选项（Radio）',
-  icon: 'radio',
+  icon: <MaterialSymbolsRadio />,
   element: <Page />,
 }, {
   path: 'title-bar',
   title: '标题栏（Title Bar）',
-  icon: 'window',
+  icon: <MaterialSymbolsWindow />,
   element: <Page />,
 }, {
   path: 'text',
   title: '文本（Text）',
-  icon: 'format_size',
+  icon: <MaterialSymbolsFormatSize />,
   element: <Page />,
 }, {
   path: 'test',
   title: '测试用',
-  icon: 'science',
+  icon: <MaterialSymbolsScience />,
   element: <Page />,
 },
 ]
--- src/components/ThemeTogger/index.tsx
+++ src/components/ThemeTogger/index.tsx
@@ -1,7 +1,8 @@
 import { useTheme } from '../../hooks'
 import { Btn } from '../Btn'
 import { TablerMoon, TablerSun, TablerSunMoon } from '@roku-ui/icons-tabler'
-export function ThemeToggle () {
+import { type ButtonProps } from '../Btn/Btn'
+export function ThemeToggle ({ ...others }: Omit<ButtonProps, 'onClick' | 'icon' >) {
   const { theme, toggleTheme } = useTheme()
   let icon = <TablerMoon />
   switch (theme) {
@@ -15,6 +16,7 @@ export function ThemeToggle () {
     <Btn
       icon
       onClick={toggleTheme}
+      {...others}
     >
       { icon }
     </Btn>
